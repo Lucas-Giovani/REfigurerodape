e34cfb331b40e7e86fc6d590b2975e48a1c7241e
Lucas-giovani <Lucas_fruck@estudante.sesisenai.org.br>
2023-06-22 09:38:14 -0300

    asd

---
+++ js/tela_inicial.js
@@ -0,0 +1,62 @@
+function Logo()
+{
+  window.location.replace("logado.php")
+};
+
+/* Abre e fecha menu lateral em modo mobile */
+
+const menuMobile = document.querySelector(".menu-mobile");
+const body = document.querySelector("body");
+
+menuMobile.addEventListener("click", () => {
+    menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list", "bi-x")
+    : menuMobile.classList.replace("bi-x", "bi-list");
+    body.classList.toggle("menu-nav-active");
+});
+
+/* Fecha o menu quando clicar em algum item e muda o icone para list */
+
+const navitem = document.querySelectorAll('.nav-item')
+
+navitem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
+});
+
+const nav = document.querySelector('nav');
+
+nav.addEventListener('touchstart', function() {
+  nav.classList.toggle('hover');
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+};
